a490bb99d4987fb015a6218dbfb4f4fa580cdfa8
apil <apilpal@ymail.com>
2015-09-04 20:27:37 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+printf("Enter two no.");
+scanf("%d %d",&i,&j);
+printf("Add.. %d",i+j);
+printf("Sub.. %d",i-j);
+printf("Mul.. %d",i*j);
+printf("Div.. %d",i/j);
+return 0;
+}
