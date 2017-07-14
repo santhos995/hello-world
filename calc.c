8ab106fda0bba1bb2b1e5984e1b5ef1afef1dc55
apilpal <apilapl@ymail.com>
2017-07-15 02:01:57 +0530

    com

--- calc.c
+++ calc.c
@@ -2,12 +2,12 @@
 int main()
 {
 int i,j;
-printf("Enter two no.");
-scanf("%d %d\t",&i,&j);
-printf("\nAdd.. %d\n",i+j);
+printf("Enter two no.	\n");
+scanf("%d %d    ",&i,&j);
+printf("Add.. %d\n",i+j);
 printf("\nSub.. %d\n",i-j);
-printf("Mul.. %d\n",i*j);
-printf("Div.. %d\n",i/j);
+printf("\nMul.. %d\n",i*j);
+printf("\nDiv.. %d\n",i/j);
 return 0;
 /*Comment*/
 /*Comment by Mayank*/
