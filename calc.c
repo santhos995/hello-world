dcbde454928510fead837f51af77011c0b7218a1
apilpal <apilapl@ymail.com>
2017-07-15 01:51:21 +0530

    somecommen

--- calc.c
+++ calc.c
@@ -5,7 +5,7 @@ int i,j;
 printf("Enter two no.");
 scanf("%d %d\t",&i,&j);
 printf("\nAdd.. %d\n",i+j);
-printf("Sub.. %d",i-j);
+printf("\nSub.. %d\n",i-j);
 printf("Mul.. %d",i*j);
 printf("Div.. %d\n",i/j);
 return 0;
