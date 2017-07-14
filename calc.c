dd807438677e7abe1af11730fa897dc4dcbba7b0
apilpal <apilapl@ymail.com>
2017-07-15 01:27:50 +0530

    adding calc to master

---
+++ calc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+printf("Enter two no.");
+scanf("%d %d",&i,&j);
+printf("\nAdd.. %d\n",i+j);
+printf("Sub.. %d",i-j);
+printf("Mul.. %d",i*j);
+printf("Div.. %d\n",i/j);
+return 0;
+/*Comment*/
+/*Comment by Mayank*/
+}
