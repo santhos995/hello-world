3a4026e5946730ae5cec08004bfbf0d13709e75f
Sunny Wang <qwang135@myseneca.ca>
2018-12-07 21:37:42 -0500

    make calculator.c run

--- calculator.c
+++ calculator.c
@@ -2,14 +2,15 @@
 int main()
 {
 int i,j;
-printf("Enter two no.");
+printf("Enter two numbers(separate by space):");
 scanf("%d %d",&i,&j);
 printf("\nAdd.. %d\n",i+j);
-printf("Sub.. %d",i-j);
-printf("Mul.. %d",i*j);
+printf("Sub.. %d\n",i-j);
+printf("Mul.. %d\n",i*j);
 printf("Div.. %d\n",i/j);
-printf("\nDone!!\n", );
+printf("\nDone!!\n" );
 return 0;
 /*Comment Andrew/
 /*Comment by Mayank*/
+/* Comment by Sunny*/
 }
