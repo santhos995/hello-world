b754e1c18eede7269b9d956a67011565d16c237d
Santhosh Kumar <santhos995@gmail.com>
2021-03-09 10:59:26 +0530

    added js file for hotpatch

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+
+console.log(add(5, 3));
+console.log(subtract(5, 3));
+console.log(multiply(5, 3));
